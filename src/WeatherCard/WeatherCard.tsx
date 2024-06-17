01c669a094d04b42388c2e67f11553a5e6489674
Uriel Cabrera Salinas <cabrera.cs.personal@outlook.com>
2024-06-16 22:52:58 -0600

    CHORE: escritura de la ruta de los svgs

--- src/WeatherCard/WeatherCard.tsx
+++ src/WeatherCard/WeatherCard.tsx
@@ -6,7 +6,7 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ type, weather, isCelsius }) =
     return (
         <div className="card">
             <h2>{type} Weather</h2>
-            <img style={{width: '15rem'}} src={`../public/weather_icons/${weather.wx_icon}.svg`} alt={`${weather.wx_icon}`} />
+            <img style={{width: '15rem'}} src={`/weather_icons/${weather.wx_icon}.svg`} alt={`${weather.wx_icon}`} />
             <p><strong>IATA code:</strong> {weather.location_iata_code}</p>
             <p><strong>Location:</strong> {weather.location}</p>
             <p><strong>{weather.wx_desc}</strong></p>
